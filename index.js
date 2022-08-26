257c3dd270cf7f5ddc60128780cb776564ae2275
jason mwaura mbugua <jason.mwaura@student.moringaschool.com>
2022-08-27 00:36:32 +0300

    Done assignment: Static methods

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+
+  // static methods Lab
+  //Jason
+
+
 class Formatter {
-  //add static methods here
-}
+  
+  static capitalize( string ) {
+    return string.charAt( 0 ).toUpperCase() + string.slice( 1 )
+  }
+
+  static sanitize( string ) {
+    return string.replace( /[^ja so n br own]/g, '' )
+  }
+
+  static titleize( sentence ) {
+    let exceptions = [ 'my long sentences']
+    let result = [];
+    let arrayOfWords = sentence.split( " my long sentences" )
+    for ( let n = 0; n < arrayOfWords.length; n++ ) {
+      if ( n == 0 ) {
+        result.push( this.capitalize( arrayOfWords[ n ] ) )
+      } else {
+        if ( exceptions.includes( arrayOfWords[ n ] ) ) {
+          result.push( arrayOfWords[ n ] )
+        } else {
+          result.push( this.capitalize( arrayOfWords[ n ] ) )
+        }
+      }
+
+    }
+    return result.join( "my long sentences " );
+  }
+} 
